0b1616de3b27827e1a5496f2fd1b9bc45b7159ca
Слизова Мария Сергеевна <slizova_ms@MacBook-Pro-User-8.local>
2026-07-10 13:58:52 +0300

    Add PWA support

---
+++ src/registerServiceWorker.js
@@ -0,0 +1,12 @@
+// src/registerServiceWorker.js
+export function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .catch((error) => {
+          console.log('Service worker registration failed:', error);
+        });
+    });
+  }
+}
