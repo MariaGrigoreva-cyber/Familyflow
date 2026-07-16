5778e5122680fbfc361a61ccea31f2bd22b2057f
Слизова Мария Сергеевна <slizova_ms@MacBook-Pro-User-8.local>
2026-07-16 13:31:49 +0300

    fix: unregister service worker, cloud sync 409 contract

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { registerServiceWorker } from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
+unregister();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
